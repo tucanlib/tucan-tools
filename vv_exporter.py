035163b5354ad73d2769f49573aa073b8cc444ad
David Gengenbach <davidgengenbach@gmx.de>
2017-03-01 18:35:20 +0100

    :)

--- vv_exporter.py
+++ vv_exporter.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 
-'''
-Why does this have to be such a pain :/
-BeautifulSoup and me won't become friends, it's misunderstanding me too stubbornly...
-'''
-
 import re
 import json
 import helper
 from bs4 import BeautifulSoup
 
-BLACKLIST=(
+BLACKLIST = (
     'Weitere Veranstaltungen',
     'Leistungen für den Masterstudiengang',
     'Zusätzliche Leistungen',
@@ -24,18 +19,23 @@ BLACKLIST=(
 
 BASE_URL = helper.get_tucan_baseurl()
 
+
 def details_from_element(element):
     link = element.find('a')
     title = link.text.strip()
     link = BASE_URL + link.attrs['href']
-    return  {'title': title, 'link': link, 'isParent': "PRGNAME=REGISTRATION" in link,  'isModule': "PRGNAME=MODULEDETAILS" in link, 'children': []}
+    return {'title': title, 'link': link, 'isParent': "PRGNAME=REGISTRATION" in link,  'isModule': "PRGNAME=MODULEDETAILS" in link, 'children': []}
+
 
 def get_all_links(page):
     SELECTOR = '#pageContent ul li, #pageContent table tr'
-    links = [details_from_element(x) for x in page.soup.select(SELECTOR) if x.text.strip() not in BLACKLIST and len(x.select('a')) > 0]
+    links = [details_from_element(x) for x in page.soup.select(
+        SELECTOR) if x.text.strip() not in BLACKLIST and len(x.select('a')) > 0]
     return links
 
 # ....
+
+
 def sanitize_detail(detail):
     replacements = [
         ('\t', ''),
@@ -69,9 +69,11 @@ def sanitize_detail(detail):
     detail['details'] = detail_text
     return detail
 
+
 def extract_module_details(html):
     details_raw = html.select('#pageContent table:nth-of-type(1) .tbdata td')
     return [sanitize_detail({"title": x.split('</b>')[0].strip(), "details": x.split('</b>')[1].strip()}) for x in str(details_raw).split('<b>')[1:]]
+
 
 def extract_cp(link):
     for detail in link['details']:
@@ -84,8 +86,10 @@ def extract_cp(link):
             return cp
     return 0
 
+
 def print_link(link):
-    print(('\t' * link['depth']) + '> {}'.format(link['title']) )
+    print(('\t' * link['depth']) + '> {}'.format(link['title']))
+
 
 def crawl(browser, link):
     if link['isParent']:
@@ -101,25 +105,28 @@ def crawl(browser, link):
         print_link(link)
     return link
 
-def walk_modules(vv, fn, only_children = True):
+
+def walk_modules(vv, fn, only_children=True):
     if len(vv['children']) == 0 or not only_children:
         vv = fn(vv)
     for child in vv['children']:
         child = walk_modules(child, fn, only_children)
     return vv
 
+
 def get_vv(browser, start_page, base_url):
     # anmeldung page
     anmeldung_page_link = base_url + start_page.soup.select('li[title="Anmeldung"] a')[0].attrs['href']
 
-    vv = crawl(browser, {'title': 'Start', 'link': anmeldung_page_link, 'isParent': True,  'isModule': False, 'children': [], "depth": 0})
+    vv = crawl(browser, {'title': 'Start', 'link': anmeldung_page_link,
+                         'isParent': True,  'isModule': False, 'children': [], "depth": 0})
 
     def remove_unneccesary_data(module):
         for e in ['link', 'isParent', 'depth', 'isModule']:
             del module[e]
         return module
 
-    vv = walk_modules(vv, remove_unneccesary_data, only_children = False)
+    vv = walk_modules(vv, remove_unneccesary_data, only_children=False)
     return vv['children']
 
 if __name__ == '__main__':
